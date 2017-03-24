f4eb7af8ca252606a5166dbd0da3af8e5b89ee4a
nathan <njgingrich@gmail.com>
2017-03-24 13:53:38 -0400

    added flowtype checking

--- src/client/game.js
+++ src/client/game.js
@@ -4,27 +4,37 @@ import Player from './player'
 
 class Game {
   player: Player
-  canvas: HTMLElement
-  c: CanvasRenderingContext2D
+  canvas: ?HTMLElement
+  c: ?CanvasRenderingContext2D
   WIDTH: number
   HEIGHT: number
 
   constructor() {
     this.player = new Player(395, 295)
-
-    this.canvas = document.getElementById('canvas')
-    this.c = this.canvas.getContext('2d')
     this.WIDTH = 800
     this.HEIGHT = 600
-    this.canvas.width = this.WIDTH
-    this.canvas.height = this.HEIGHT
 
-    this.loop = this.loop.bind(this)
+    this.canvas = document.getElementById('canvas')
+    if (this.canvas != null && this.canvas instanceof HTMLCanvasElement) {
+      this.c = this.canvas.getContext('2d')
+    }
+
+    if (this.canvas == null) {
+      this.canvas = new HTMLCanvasElement()
+    }
+    if (this.canvas instanceof HTMLCanvasElement) {
+      this.canvas.width = this.WIDTH
+      this.canvas.height = this.HEIGHT
+    }
+
+    (this: any).loop = this.loop.bind(this)
     setInterval(this.loop, 20)
   }
 
   loop() {
-    this.c.clearRect(0, 0, this.WIDTH, this.HEIGHT)
+    if (this.c != null) {
+      this.c.clearRect(0, 0, this.WIDTH, this.HEIGHT)
+    }
 
     this.player.drawPlayer(this.c)
   }
