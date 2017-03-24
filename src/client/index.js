6bbe5b7ebf112dbdfb4409350c397227d540609c
nathan <njgingrich@gmail.com>
2017-03-24 14:56:12 -0400

    testing

--- src/client/index.js
+++ src/client/index.js
@@ -7,7 +7,7 @@ import { APP_CONTAINER_SELECTOR } from '../shared/config'
 const app: any = document.querySelector(APP_CONTAINER_SELECTOR)
 app.innerHTML = `
 <div class="container">
-  <canvas id="canvas" tabindex="1"></canvas>
+  <canvas id="canvas" tabindex=1></canvas>
 </div>
 `
 
