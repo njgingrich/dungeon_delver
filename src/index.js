91d8cb1f97f32943660fca669661453bf780baa0
nathan <njgingrich@gmail.com>
2017-03-23 23:47:48 -0400

    set up with babel and ESLint

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Player from './player'
+
+const me = new Player('Doug')
+console.log(me.message())
---
+++ src/player.js
@@ -0,0 +1,11 @@
+class Player {
+  constructor(name) {
+    this.name = name
+  }
+
+  message() {
+    return `hey my names ${this.name}`
+  }
+}
+
+export default Player
