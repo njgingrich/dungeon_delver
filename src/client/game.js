69424249b82afff215ebf41b24432def95cdfef7
nathan <njgingrich@gmail.com>
2017-03-25 00:44:02 -0400

    installed pixi and basic setup

--- src/client/game.js
+++ src/client/game.js
@@ -1,11 +1,19 @@
 // @flow
 
+import * as Pixi from 'pixi.js'
 import Player from './player'
+import { APP_CONTAINER_SELECTOR } from '../shared/config'
+
+const Container = Pixi.Container
+const autoDetectRenderer = Pixi.autoDetectRenderer
+const loader = Pixi.loader
+const resources = Pixi.loader.resources
+const Sprite = Pixi.Sprite
 
 class Game {
   player: Player
-  canvas: ?HTMLElement
-  c: ?CanvasRenderingContext2D
+  renderer: any
+  stage: any
   WIDTH: number
   HEIGHT: number
 
@@ -13,35 +21,61 @@ class Game {
     this.player = new Player(395, 295)
     this.WIDTH = 800
     this.HEIGHT = 600
+    this._bind()
+    this.renderer = autoDetectRenderer(800, 600)
+    // flow-disable-next-line
+    document.querySelector(APP_CONTAINER_SELECTOR).appendChild(this.renderer.view)
 
-    this.canvas = document.getElementById('canvas')
-    if (this.canvas != null && this.canvas instanceof HTMLCanvasElement) {
-      this.c = this.canvas.getContext('2d')
-    }
-
-    if (this.canvas == null) {
-      this.canvas = new HTMLCanvasElement()
-    }
-    if (this.canvas instanceof HTMLCanvasElement) {
-      this.canvas.width = this.WIDTH
-      this.canvas.height = this.HEIGHT
-    }
-
-    (this: any).loop = this.loop.bind(this)
-    setInterval(this.loop, 20)
+    this.stage = new Container()
+    this._preload()
   }
 
-  loop() {
-    if (this.c != null) {
-      this.c.clearRect(0, 0, this.WIDTH, this.HEIGHT)
+  _bind() {
+    (this: any)._setup = this._setup.bind(this)
+  }
+
+  _preload() {
+    loader
+      .add('static/spritesheet.png')
+      .load(this._setup)
+  }
+
+  _setup() {
+    const sprite = new Sprite(
+      resources['static/spritesheet.png'].texture
+    )
+    console.log('stage: ', this.stage)
+    this.stage.addChild(sprite)
+    this.renderer.render(this.stage)
+  }
+
+  /*
+  _drawHud() {
+    this.c.fillStyle = 'rgb(61, 38, 24)'
+    this.c.fillRect(0, 0, 176, this.HEIGHT)
+
+    // draw inventory squares
+    const squarePos = []
+    for (let i = 1; i <= 3; i++) {
+      squarePos.push({
+        x: 16,
+        y: this.HEIGHT - ((16 + 64) * i)
+      })
+      squarePos.push({
+        x: 96,
+        y: this.HEIGHT - ((16 + 64) * i)
+      })
     }
-
-    this.player.drawPlayer(this.c)
+    squarePos.forEach((sq) => {
+      this._drawInventorySquare(sq.x, sq.y)
+    })
   }
 
-  run() {
-    this.WIDTH = 800
+  _drawInventorySquare(x: number, y: number) {
+    this.c.fillStyle = 'rgb(35, 22, 14)'
+    this.c.fillRect(x, y, 64, 64)
   }
+  */
 }
 
 export default Game
--- src/client/index.js
+++ src/client/index.js
@@ -2,14 +2,11 @@
 
 import 'babel-polyfill'
 import Game from './game'
-import { APP_CONTAINER_SELECTOR } from '../shared/config'
+import { APP_NAME, APP_CONTAINER_SELECTOR } from '../shared/config'
 
 const app: any = document.querySelector(APP_CONTAINER_SELECTOR)
 app.innerHTML = `
-<div class="container">
-  <canvas id="canvas" tabindex=1></canvas>
-</div>
+<h1>${APP_NAME}</h1>
 `
 
 const game = new Game()
-game.run()
